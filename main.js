882ef66dfa5dc54ae66db346bc867ed777e3987b
Choper03 <csutucdeleon@gmail.com>
2023-07-28 21:49:57 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+function Ejercicio1(){
+
+        var enlace = "https://ejercicio1-rose.vercel.app/";
+        window.open(enlace, '_blank');
+}
+
+function Ejercicio2(){
+
+    var enlace = "https://ejercicio2-two.vercel.app/";
+    window.open(enlace, '_blank');
+}
+
+function Ejercicio3(){
+
+    var enlace = "https://ejercicio3-snowy.vercel.app/";
+    window.open(enlace, '_blank');
+}
+
+function Ejercicio4(){
+
+    var enlace = "https://ejercicio4-six.vercel.app/";
+    window.open(enlace, '_blank');
+}
+
+
+
+function Ejercicio5(){
+    alert("No esta hecha xd")
+}
+
+function Ejercicio6(){
+
+var enlace = "https://ejercicio6-six.vercel.app/";
+window.open(enlace, '_blank');
+}
+
+function Ejercicio7(){
+
+var enlace = "https://ejercicio7-nu.vercel.app/";
+window.open(enlace, '_blank');
+}
+
+function Ejercicio8(){
+
+var enlace = "https://ejercicio8-amber.vercel.app/";
+window.open(enlace, '_blank');
+}
